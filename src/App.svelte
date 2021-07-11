4763fdc82830b04f9b9ff9321b28758e02ed0d9b
mrLuisFer <mrluisfeer@gmail.com>
2021-07-11 13:23:31 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Card from './components/Card.svelte'
+</script>
+
+<main>
+	<Card />
+</main>
+
+<style type='text/scss' lang='scss'>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<div>
+    <p> This is a card </p>
+</div>
+
+<style>
+</style>
